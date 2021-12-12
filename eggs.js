f73e649f0dce734a1532853b61abded9e48b82b5
stupid cat <cat@blargbot.xyz>
2021-12-11 18:11:48 -0700

    fix indentation

--- eggs.js
+++ eggs.js
@@ -1,5 +1,5 @@
 const matchers = {
-  // Returns true if the seed includes all terms
+	// Returns true if the seed includes all terms
 	all(terms, seed) {
 		for (const term of terms) {
 			if (!seed.includes(term)) {
@@ -30,59 +30,59 @@ const matchers = {
 }
 
 const mutations = {
-  // Sets the background image of the page
-  background(value) {
-    document.body.style.background = value;
-  },
+	// Sets the background image of the page
+	background(value) {
+		document.body.style.background = value;
+	},
 
-  // Adds a style element
-  css(value) {
-    const element = document.createElement('style');
+	// Adds a style element
+	css(value) {
+		const element = document.createElement('style');
 
-    element.innerText = value;
+		element.innerText = value;
 
-    document.head.appendChild(element);
-  }
+		document.head.appendChild(element);
+	}
 }
 
 function findEgg(eggs) {
 	const seed = urlParams.get("seed").toLowerCase();
 
 	if (seed) {
-    for (const egg of eggs) {
-      if (!egg.match) continue;
+		for (const egg of eggs) {
+			if (!egg.match) continue;
 
-      for (const key of Object.keys(egg.match)) {
-        if (matchers.hasOwnProperty(key) && typeof matchers[key] === 'function') {
-          if (matchers[key](egg.match[key], seed) === true) {
-            handleEgg(egg);
-          }
-        }
-      }
+			for (const key of Object.keys(egg.match)) {
+				if (matchers.hasOwnProperty(key) && typeof matchers[key] === 'function') {
+					if (matchers[key](egg.match[key], seed) === true) {
+						handleEgg(egg);
+					}
+				}
+			}
 		}
 	}
 }
 
 function handleEgg(egg) {
-  if (egg.mutate) {
-    for (const key of Object.keys(egg.mutate)) {
-      if (mutations.hasOwnProperty(key) && typeof mutations[key] === 'function') {
-        mutations[key](egg.mutate[key]);
-      }
-    }
-  }
+	if (egg.mutate) {
+		for (const key of Object.keys(egg.mutate)) {
+			if (mutations.hasOwnProperty(key) && typeof mutations[key] === 'function') {
+				mutations[key](egg.mutate[key]);
+			}
+		}
+	}
 }
 
 (async () => {
-  const response = await fetch('./eggs.json');
+	const response = await fetch('./eggs.json');
 
-  if (response.ok) {
-    const eggs = await response.json();
+	if (response.ok) {
+		const eggs = await response.json();
 
-    const egg = findEgg(eggs);
+		const egg = findEgg(eggs);
 
-    if (egg) {
-      handleEgg(egg);
-    }
-  }
+		if (egg) {
+			handleEgg(egg);
+		}
+	}
 })();
